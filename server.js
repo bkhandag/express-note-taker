b667676aa794950dafe85a614917f8dfbb7e8a7e
bkhandag <khandagale.b.g@gmail.com>
2022-05-07 14:28:33 -0700

    changed port to enable heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const notesDB = require('./db/db.json');
 const uuid = require('./helpers/uuid');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
